0fb05f635ae81633880786fb71774bedd088a702
Lou Foster <lf6f@virginia.edu>
2019-07-17 15:11:58 -0400

    remove test error message

--- frontend/src/store/plugins/version.js
+++ frontend/src/store/plugins/version.js
@@ -1,11 +1,10 @@
 import axios from 'axios'
 
 // Plugin to watch for version changes and reload
-const versionCheker = (store) => {
+const versionCheker = (_store) => {
    var currBuild = "unknown"
    axios.get("/version").then((response) => {
       currBuild = response.data.build
-      store.commit('setError', "this is a test error!")
       setInterval(() => {
          axios.get("/version").then((ckResp) => {
             if (currBuild!= ckResp.data.build) {
