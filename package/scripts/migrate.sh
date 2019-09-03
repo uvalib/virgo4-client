6be7180197fa201ffe99b94337bb1a13fc70128c
UVADave <dpg3k@virginia.edu>
2019-09-03 14:32:17 -0400

    Call the migration tool

--- package/scripts/migrate.sh
+++ package/scripts/migrate.sh
@@ -2,7 +2,11 @@
 # run any necessary migrations
 #
 
-# use $V4_DB_HOST $V4_DB_PORT $V4_DB_NAME $V4_DB_USER $V4_DB_PASS
+# run the migrations
+bin/migrate -path db -verbose -database postgres://$V4_DB_USER:$V4_DB_PASS@$V4_DB_HOST:$V4_DB_PORT/$V4_DB_NAME up
+
+# return the status
+exit $?
 
 #
 # end of file
