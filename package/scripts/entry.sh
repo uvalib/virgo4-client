fd31208ba89576bada7f8300100693fba311a4a5
UVADave <dpg3k@virginia.edu>
2019-09-04 10:11:18 -0400

    Add shebang cos docker does not default to a specific interpreter

--- package/scripts/entry.sh
+++ package/scripts/entry.sh
@@ -1,6 +1,13 @@
+#!/usr/bin/env bash
+#
 # run application
+#
 
+# run the server
 cd bin; ./v4srv -search $V4_SEARCH_API -ils $V4_ILS_API -dbhost $V4_DB_HOST -dbport $V4_DB_PORT -dbname $V4_DB_NAME -dbuser $V4_DB_USER -dbpass $V4_DB_PASS
+
+# return the status
+exit $?
 
 #
 # end of file
--- package/scripts/migrate.sh
+++ package/scripts/migrate.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 #
 # run any necessary migrations
 #
