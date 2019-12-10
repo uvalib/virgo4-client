01d9f2d63f637f10676b13d0d2a95d2f30bd511a
Christopher <cmw5mc@virginia.edu>
2019-12-10 14:39:25 -0500

    VIRGONEW-953

--- frontend/src/components/SearchFilters.vue
+++ frontend/src/components/SearchFilters.vue
@@ -2,7 +2,7 @@
    <div class="filters">
       <div class="filters-head clearfix">
          <span class="title">Applied Filters</span>
-         <span v-if="hasFilter(resultsIdx)" @click="clearClicked" class="clear">Clear All</span>
+         <span v-if="hasFilter(resultsIdx)" @click="clearClicked" class="clear pure-button pure-button-primary">Clear All</span>
       </div>
       <template v-if="hasFilter(resultsIdx)">
          <dl class="filter-display">
@@ -11,7 +11,7 @@
                <dd :key="idx" class="filter">{{formatValues(values)}}</dd>
             </template>
          </dl>
-      </template> 
+      </template>
       <div v-else class="no-filter">
          <span>None</span>
       </div>
@@ -36,7 +36,7 @@ export default {
          return this.selectedResults.total
       },
       displayFilter() {
-         // display is grouped by facet, raw data is just a series of 
+         // display is grouped by facet, raw data is just a series of
          // facet_id/value pairs. Convert to display
          let out = {}
          this.allFilters(this.resultsIdx).forEach(pf=>{
@@ -65,27 +65,41 @@ export default {
 <style scoped>
 .filters {
    background: white;
-   color: #666;
-   font-size: 0.8em;
-   padding: 5px 0;
+   color: var(--uvalib-text);
+   padding: 15px 5px;
    margin-top: 5px;
 }
 .filters-head {
    font-weight: bold;
-   border-bottom: 1px solid #ccc;
+   border-bottom: 1px solid var(--uvalib-grey-light);
    margin-bottom: 5px;
-   padding-bottom: 5px;
+   padding-bottom: 10px;
 }
-dt {
+@media only screen and (max-width: 768px) {
+  dl.filter-display {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-gap: 8px;
+  }
+}
+@media only screen and (min-width: 768px) {
+  dl.filter-display {
+    display: grid;
+    grid-template-columns: 0.5fr 1fr;
+    grid-gap: 8px;
+  }
+}
+.filter-display dt {
    font-weight: bold;
+}
+.filter-display dd {
+  margin-inline-start: 0px;
 }
 .filter-display {
    margin-left: 10px;
-   font-size: 0.9em;
 }
 .filters-head .title {
    vertical-align: -webkit-baseline-middle;
-   font-size: 1.1em;
    padding-left: 10px;
 }
 .no-filter {
@@ -97,21 +111,13 @@ dt {
   display: table;
 }
 .clear {
-   background: var(--color-primary-blue);
    float: right;
    margin-right: 6px;
-   color: white;
-   font-size:0.9em;
-   padding: 5px 20px;
-   border-radius: 5px;
    cursor:pointer;
-   opacity: 0.9;
 }
 .remove-filter {
    padding-right: 10px;
-   color: #666;
-   opacity: 0.5;
-   font-size: 1.25em;
+   color: var(--uvalib-text);
    cursor: pointer;
 }
 .remove-filter:hover {
