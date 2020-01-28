edf793bbec2dd1cb1f9b57f08ff3355b9faa8ec7
Jason Jordan <jlj5aj@virginia.edu>
2020-01-27 20:17:50 -0500

    add new thesis pool migration

---
+++ backend/db/migrations/000018_add_thesis_pool.up.sql
@@ -0,0 +1,2 @@
+INSERT into sources (private_url,name,public_url) VALUES 
+   ('http://pool-solr-ws-thesis.private.staging:8080', 'thesis', 'https://pool-solr-ws-thesis-dev.internal.lib.virginia.edu');
