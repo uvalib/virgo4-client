e0a2dbbc05adc1b4d72d0843db515bd48f6ce3b7
Jason Jordan <jlj5aj@virginia.edu>
2020-02-17 19:53:09 -0500

    add images pool

---
+++ backend/db/migrations/000022_add_images_pool.up.sql
@@ -0,0 +1,2 @@
+INSERT into sources (private_url,name,public_url) VALUES 
+   ('http://pool-solr-ws-images.private.staging:8080', 'images', 'https://pool-solr-ws-images-dev.internal.lib.virginia.edu');
