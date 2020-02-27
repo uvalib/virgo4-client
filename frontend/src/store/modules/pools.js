ef9c9d44449bc1bfe627aaf27e323c2786a608a3
Lou Foster <lf6f@virginia.edu>
2020-02-27 17:01:29 -0500

    VIRGONEW-1051 first pass as usign providers data. messy

--- frontend/src/store/modules/pools.js
+++ frontend/src/store/modules/pools.js
@@ -73,6 +73,14 @@ const pools = {
          if (!attr) return true
          return attr.supported
       },
+      findProvider: (state) => (poolID, providerID) => {
+         let pool = state.list.find( p => p.id == poolID)
+         if (!pool) return {}
+         if (!pool.providers) return {}
+         let detail = pool.providers.find( p=> p.provider==providerID)
+         if (!detail) return {}
+         return detail
+      },
    },
 
    mutations: {
@@ -80,24 +88,50 @@ const pools = {
          state.lookingUp = flag
       },
       setPools(state, data) {
-         state.list = data
-         if (state.list.length == 0) {
+         if (data.length == 0) {
             state.system.fatal = "No search pools configured"
+            state.list.splice(0, state.list.length)
+            return
          }
+
+         data.forEach( p => {
+            let currIdx = state.list.findIndex(curr => curr.name == p.name  )
+            if (currIdx == -1) {
+               p.providers = []
+               state.list.push(p)    
+            }
+         })
       },
+      setPoolProviders(state, data) {
+         let pool = state.list.find(p=> p.id == data.pool)
+         pool.providers.splice(0, pool.providers.length)
+         data.providers.forEach( prov => {
+            pool.providers.push(prov)
+         })
+      }
    },
 
    actions: {
-      getPools(ctx) {
+      async getPools(ctx) {
          ctx.commit("setLookingUp", true)
          let url = ctx.rootState.system.searchAPI + "/api/pools"
-         return axios.get(url).then((response) => {
+         try {
+            let response = await axios.get(url)
             ctx.commit('setPools', response.data)
             ctx.commit("setLookingUp", false)
-         }).catch((error) => {
+
+            ctx.state.list.forEach( async p => {
+               try {
+                  let response = await axios.get(p.url+"/api/providers")
+                  ctx.commit('setPoolProviders', {pool: p.id, providers: response.data.providers})
+               } catch (_e) {
+                  // NO-OP; this is fine as some pools don't implement providers
+               }
+            })
+         } catch (error)  {
             ctx.commit('system/setFatal', "Unable to get pools: " + error.response.data, { root: true })
             ctx.commit("setLookingUp", false)
-         })
+         }
       },
    }
 }
