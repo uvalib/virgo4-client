37b6bc31df7c67bb5f9d4cad07068a5adb779b00
Lou Foster <lf6f@virginia.edu>
2020-03-12 16:00:02 -0400

    update wording on reserves success

--- frontend/src/views/CourseReserveSuccess.vue
+++ frontend/src/views/CourseReserveSuccess.vue
@@ -5,8 +5,8 @@
          <h2>Thank you for your course reserves request!</h2>
          <p>A copy of your request has been emailed to you.</p>
          <p>If we have questions regarding your request someone will contact you.</p>
-         <p>When your request has been processed, you'll be able to see your reserves by signing into your Virgo account 
-            and clicking the Course Reserves menu option.</p>
+         <p>When your request has been processed, you'll be able to see your non-video 
+            reserves by signing into your Virgo account and clicking the Course Reserves menu option.</p>
          <p>
             If the Library doesn’t own an item that you need for reserve, you may make a 
             <a href="https://www.library.virginia.edu/services/purchase-requests/" target="_blank">purchase recommendation</a>.
