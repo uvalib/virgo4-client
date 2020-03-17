4ecd2dc2759a9700aa0a147c0348f13e1fcf1051
Lou Foster <lf6f@virginia.edu>
2020-03-17 12:27:25 -0400

    clean up scan code

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -27,15 +27,23 @@
             <span @click="searchClicked" class="search pure-button pure-button-primary">Search</span>
           </div>
           <div class="advanced">
-            <span v-if="showVideo==false" @click="videoShow" class="text-button">Scan Barcode</span>
-            <StreamBarcodeReader v-if="showVideo"
-               @decode="onBarcodeDecode"
-            ></StreamBarcodeReader>
-          </div>
-          <div class="advanced">
             <span class="text-button advanced-link" @click="advancedClicked">
               Advanced Search&nbsp;<i class="fas fa-search-plus"></i>
             </span>
+          </div>
+          <div class="advanced">
+            <span v-if="showVideo==false" @click="videoShow" class="text-button">
+               Scan Barcode&nbsp;<i class="fas fa-camera"></i>
+            </span>
+            <div class="scan-wrapper" v-if="showVideo">
+               <span>Scan a barcode with the camera on your device</span>
+               <StreamBarcodeReader 
+                  @decode="onBarcodeDecode"
+               ></StreamBarcodeReader>
+               <div class="scan controls">
+                  <span @click="cancelScan" class="pure-button pure-button-primary">Cancel</span>
+               </div>
+            </div>
           </div>
           <div class="advanced">
             <router-link to="/journals">
@@ -222,15 +230,40 @@ export default {
       onBarcodeDecode(result) {
          this.showVideo =false
          this.basic = result
+         this.searchClicked()
       },
       videoShow() {
          this.showVideo = true
+      },
+      cancelScan() {
+         this.showVideo = false
       }
    }
 };
 </script>
 
 <style scoped>
+.scan-wrapper {
+   text-align: center;
+   padding: 10px; 
+   box-shadow: 0 1px 3px rgba(0, 0, 0, 0.06), 0 1px 2px rgba(0, 0, 0, 0.12);
+   border: 1px solid var(--uvalib-grey-light);
+}
+.scan-wrapper span {
+   margin: 0 0 10px 0;
+   display: inline-block;
+   color: 1px solid var(--uvalib-grey);
+}
+.scan-wrapper .scan.controls {
+   text-align: right;
+   display: block;
+   margin:0;
+   padding:0;
+   color:white;
+}
+.scan-wrapper .scan.controls .pure-button.pure-button-primary {
+   margin: 5px 0 0 0;
+}
 @media only screen and (min-width: 768px) {
   div.searching-box {
     padding: 20px 90px;
