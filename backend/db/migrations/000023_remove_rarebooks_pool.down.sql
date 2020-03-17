203dc976ec2913e30609c3e4682f598a00417c24
Jason Jordan <jlj5aj@virginia.edu>
2020-03-17 15:24:23 -0400

    remove rare books pool

---
+++ backend/db/migrations/000023_remove_rarebooks_pool.down.sql
@@ -0,0 +1,2 @@
+INSERT into sources (private_url,name,public_url) VALUES 
+   ('http://pool-solr-ws-rarebooks.private.staging:8080', 'rarebooks', 'https://pool-solr-ws-rarebooks-dev.internal.lib.virginia.edu');
