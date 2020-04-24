c71b7ac14d98fefc1108592f900d033130c6f7f9
Lou Foster <lf6f@virginia.edu>
2020-04-24 13:18:51 -0400

    roll back accidental checkin

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -67,216 +67,205 @@ import V4BarcodeScanner from "@/components/V4BarcodeScanner"
 export default {
    name: "home",
    components: {
-      SearchResults,
-      V4BarcodeScanner,
-      SearchTips,
-      AdvancedSearch,
-      V4Select,
-      Welcome,
-      SourceInfo
+     SearchResults, V4BarcodeScanner,
+     SearchTips, AdvancedSearch,
+     V4Select, Welcome, SourceInfo
    },
-   beforeRouteUpdate(to, _from, next) {
-      if (to.query.mode == "advanced") {
-         this.$store.commit("query/setAdvancedSearch");
+   beforeRouteUpdate (to, _from, next) {
+      if (to.query.mode == 'advanced') {
+         this.$store.commit("query/setAdvancedSearch")
       } else {
-         this.$store.commit("query/setBasicSearch");
+         this.$store.commit("query/setBasicSearch")
       }
-      next();
+      next()
    },
    data: function() {
       return {
          showVideo: false
-      };
+      }
    },
    computed: {
       ...mapState({
          searching: state => state.searching,
          searchMode: state => state.query.mode,
          translateMessage: state => state.system.translateMessage,
-         restoreMessage: state => state.query.restoreMessage
+         restoreMessage: state => state.query.restoreMessage,
       }),
       ...mapGetters({
-         rawQueryString: "query/string",
-         hasResults: "hasResults",
-         hasTranslateMessage: "system/hasTranslateMessage",
-         isSignedIn: "user/isSignedIn",
-         sources: "pools/sortedList",
-         selectedResults: "selectedResults"
+        rawQueryString: 'query/string',
+        hasResults: 'hasResults',
+        hasTranslateMessage: 'system/hasTranslateMessage',
+        isSignedIn: 'user/isSignedIn',
+        sources: 'pools/sortedList',
+        selectedResults: 'selectedResults',
       }),
-      ...mapFields("query", ["basic", "basicSearchScope"]),
+      ...mapFields('query',[
+        'basic','basicSearchScope'
+      ]),
       searchScopes() {
-         let out = [{ name: "All Resource Types", id: "all" }];
-         return out.concat(this.sources);
+        let out = [{name: 'All Resource Types', id: 'all'}]
+        return out.concat(this.sources)
       },
       basicSearch() {
-         return this.searchMode == "basic";
+        return this.searchMode == "basic"
       },
       isHomePage() {
-         return this.$router.currentRoute == "/";
+         return this.$router.currentRoute == '/'
       },
       // This restore refers to a Saved Search
       isRestore() {
-         return (
-            this.$route.params !== undefined &&
-            this.$route.params.id !== undefined &&
-            this.$route.params.id != ""
-         );
+         return ( this.$route.params !== undefined &&
+              this.$route.params.id !== undefined &&
+              this.$route.params.id != "")
       }
    },
-   // mounted: function() {
-   //    // The search page has been mounted either by direct
-   //    // URL access or by a browser back action. Put initialize
-   //    // the query module for searching (as opposed to browsing)
-   //    this.$store.dispatch("query/initSearchMode");
-   // },
+   mounted: function() {
+      // The search page has been mounted either by direct
+      // URL access or by a browser back action. Put initialize
+      // the query module for searching (as opposed to browsing)
+      this.$store.dispatch("query/initSearchMode")
+   },
    created: function() {
-      this.searchCreated();
+      this.searchCreated()
    },
    methods: {
       async searchCreated() {
          // Config and pools are needed for this page.
-         await this.$store.dispatch("system/getConfig");
-         await this.$store.dispatch("pools/getPools");
+         await this.$store.dispatch("system/getConfig")
+         await this.$store.dispatch('pools/getPools')
 
          // When restoring a saved search, the call will be /search/:token
-         if (this.isRestore) {
-            this.restoreSavedSearch(this.$route.params.id);
-            this.$store.commit("restore/clearAll");
-            return;
-         } else if (this.$store.getters["restore/hasPreviousSearch"]) {
-            this.restorePreviousSearch();
+         if ( this.isRestore) {
+            this.restoreSavedSearch(this.$route.params.id)
+            this.$store.commit("restore/clearAll")
+            return
+         } else if(this.$store.getters['restore/hasPreviousSearch']) {
+           this.restorePreviousSearch()
          }
       },
 
-      async restoreSavedSearch(token) {
-         this.$store.dispatch("query/loadSearch", token);
+      async restoreSavedSearch( token ) {
+         this.$store.dispatch("query/loadSearch", token)
       },
 
       // Look for the bookmark cookie. If found, it is an indicator that a user tried to bookmark
       // an item while not signed in. If there is now a signed in user, replay the search,
       // scroll to the target hit and open bookmark popup
       async restorePreviousSearch() {
-         try {
-            // clear and cancel if not signed in
-            if (!this.$store.getters["user/isSignedIn"]) {
-               return;
-            }
-            await this.$store.dispatch("restore/loadSearch");
 
-            this.showBookmarkTarget();
-         } finally {
-            this.$store.commit("restore/clearAll");
-         }
+        try {
+          // clear and cancel if not signed in
+          if( !this.$store.getters['user/isSignedIn']) {
+            return
+          }
+          await this.$store.dispatch("restore/loadSearch")
+
+          this.showBookmarkTarget()
+        } finally {
+          this.$store.commit("restore/clearAll")
+        }
       },
       showBookmarkTarget() {
-         let bmRestore = this.$store.getters["restore/bookmarkData"];
+        let bmRestore = this.$store.getters['restore/bookmarkData']
 
-         if (!bmRestore.recordId) {
-            return;
-         }
+        if (!bmRestore.recordId) {return}
 
-         let identifier = bmRestore.recordId;
-         let bmData = { pool: bmRestore.poolName, data: null };
-         if (bmRestore.groupParent) {
-            let sel = `.hit[data-identifier="${bmRestore.groupParent}"]`;
-            let tgtEle = document.body.querySelector(sel);
-            tgtEle.scrollIntoView();
+        let identifier = bmRestore.recordId
+        let bmData = {pool: bmRestore.poolName, data: null}
+        if ( bmRestore.groupParent) {
+          let sel = `.hit[data-identifier="${bmRestore.groupParent}"]`
+          let tgtEle = document.body.querySelector(sel)
+          tgtEle.scrollIntoView()
 
-            // find the item in the group that was targeted for a bookmark
-            let parent = this.selectedResults.hits.find(
-               r => r.identifier == bmRestore.groupParent
-            );
-            bmData.data = parent.group.find(r => r.identifier == identifier);
+          // find the item in the group that was targeted for a bookmark
+          let parent = this.selectedResults.hits.find( r=> r.identifier == bmRestore.groupParent)
+          bmData.data = parent.group.find( r=> r.identifier == identifier)
 
-            // The group accordion watches this value. When set, the accordion will auto-expand
-            this.$store.commit("setAutoExpandGroupID", bmRestore.groupParent);
+          // The group accordion watches this value. When set, the accordion will auto-expand
+          this.$store.commit('setAutoExpandGroupID', bmRestore.groupParent)
 
-            // once the group is expanded, scroll to the target group item
-            setTimeout(() => {
-               sel = `.group-hit[data-identifier="${identifier}"]`;
-               tgtEle = document.body.querySelector(sel);
-               this.scrollToItem(tgtEle);
-            }, 300);
-         } else {
-            console.log("scroll to " + identifier);
-            let sel = `.hit[data-identifier="${identifier}"]`;
-            let tgtEle = document.body.querySelector(sel);
-            this.scrollToItem(tgtEle);
-            bmData.data = this.selectedResults.hits.find(
-               r => r.identifier == identifier
-            );
-         }
-         this.$store.commit("restore/clearAll");
-         this.$store.commit("bookmarks/showAddBookmark", bmData);
+          // once the group is expanded, scroll to the target group item
+          setTimeout( ()=>{
+            sel = `.group-hit[data-identifier="${identifier}"]`
+            tgtEle = document.body.querySelector(sel)
+            this.scrollToItem(tgtEle)
+          }, 300)
+
+        } else {
+          let sel = `.hit[data-identifier="${identifier}"]`
+          let tgtEle = document.body.querySelector(sel)
+          this.scrollToItem(tgtEle)
+          bmData.data = this.selectedResults.hits.find( r=> r.identifier == identifier)
+        }
+        this.$store.commit("restore/clearAll")
+        this.$store.commit("bookmarks/showAddBookmark", bmData)
       },
 
-      scrollToItem(tgtEle) {
-         let nav = document.getElementById("v4-navbar");
-         var headerOffset = nav.offsetHeight;
-         var elementPosition = tgtEle.getBoundingClientRect().top;
-         var offsetPosition = elementPosition - headerOffset;
-         window.scrollTo({
-            top: offsetPosition,
-            behavior: "smooth"
-         });
+      scrollToItem( tgtEle ) {
+        let nav = document.getElementById("v4-navbar")
+        var headerOffset = nav.offsetHeight
+        var elementPosition = tgtEle.getBoundingClientRect().top
+        var offsetPosition = elementPosition - headerOffset
+        window.scrollTo({
+          top: offsetPosition,
+          behavior: "smooth"
+        })
       },
 
       searchClicked() {
-         this.$store.commit("resetSearchResults");
-         this.$store.commit("filters/reset");
-         this.$store.dispatch("searchAllPools");
+        this.$store.commit('resetSearchResults')
+        this.$store.commit('filters/reset')
+        this.$store.dispatch("searchAllPools")
       },
 
-      barcodeScanned(barcode) {
-         this.basic = barcode;
-         this.searchClicked();
-      }
+      barcodeScanned( barcode ) {
+         this.basic = barcode
+         this.searchClicked()
+      },
    }
 };
 </script>
 
 <style scoped>
 @media only screen and (min-width: 768px) {
-   div.searching-box {
-      padding: 20px 90px;
-   }
+  div.searching-box {
+    padding: 20px 90px;
+  }
 }
 @media only screen and (max-width: 768px) {
-   div.searching-box {
+  div.searching-box {
       width: 95%;
       padding: 20px 0;
-      margin-top: 30%;
-   }
-   div.tips-container {
-      display: none;
-   }
-   ::-webkit-input-placeholder {
-      color: transparent;
-   }
-   :-moz-placeholder {
-      /* Firefox 18- */
-      color: transparent;
-   }
-   ::-moz-placeholder {
-      /* Firefox 19+ */
-      color: transparent;
-   }
-   :-ms-input-placeholder {
-      color: transparent;
-   }
+      margin-top:30%;
+  }
+  div.tips-container {
+    display: none;
+  }
+  ::-webkit-input-placeholder {
+    color:transparent;
+  }
+  :-moz-placeholder { /* Firefox 18- */
+    color:transparent;
+  }
+  ::-moz-placeholder {  /* Firefox 19+ */
+    color:transparent;
+  }
+  :-ms-input-placeholder {
+    color:transparent;
+  }
 }
 .controls {
-   padding: 10px 0;
-   display: flex;
-   flex-flow: row wrap;
-   align-items: center;
-   justify-content: flex-start;
+  padding: 10px 0;
+  display: flex;
+  flex-flow: row wrap;
+  align-items: center;
+  justify-content: flex-start;
 }
 .controls .search.v4-button {
    margin-left: auto;
 }
-.controls > * {
-   flex: 0 1 auto;
+.controls  > * {
+  flex: 0 1 auto;
 }
 
 .home {
@@ -284,54 +273,53 @@ export default {
    position: relative;
 }
 h2 {
-   margin-top: 30px;
-   color: #444;
+  margin-top:30px;
+  color: #444;
 }
 .search-panel {
-   margin: 0 auto 0 auto;
-   text-align: center;
-   max-width: 800px;
-   padding: 10px 2vw 10px 2vw;
-   font-size: 0.95em;
+  margin: 0 auto 0 auto;
+  text-align: center;
+  max-width: 800px;
+  padding: 10px 2vw 10px 2vw;
+  font-size: 0.95em;
 }
 .basic-search {
-   display: flex;
-   flex-flow: row nowrap;
-   align-items: flex-start;
+  display: flex;
+  flex-flow: row nowrap;
+  align-items: flex-start;
 }
-#app .pure-form div.basic-search input[type="text"].basic {
-   font-size: 1.15em;
-   padding: 0.5vw 0.75vw;
-   outline: none;
-   border: 1px solid #ccc;
-   margin: 0;
-   border-left: 0;
-   border-radius: 0 5px 5px 0;
-   flex: 1 1 auto;
-   min-width: 100px;
+#app .pure-form div.basic-search  input[type=text].basic {
+  font-size: 1.15em;
+  padding: 0.5vw 0.75vw;
+  outline: none;
+  border: 1px solid #ccc;
+  margin: 0;
+  border-left: 0;
+  border-radius: 0 5px 5px 0;
+  flex: 1 1 auto;
+  min-width: 100px;
 }
 div.advanced {
-   margin-top: 10px;
-   font-size: 1em;
-   text-align: right;
+  margin-top: 10px;
+  font-size: 1em;
+  text-align: right;
 }
 div.tips-container {
-   position: absolute;
-   font-size: 1em;
-   top: 15px;
-   right: 15px;
+  position: absolute;
+  font-size: 1em;
+  top: 15px;
+  right: 15px;
 }
 div.translate-message {
-   margin: 5px 0 15px 0;
-   font-size: 0.85em;
+  margin: 5px 0 15px 0;
+  font-size: 0.85em;
 }
 p.session {
-   display: inline-block;
-   background: var(--uvalib-blue-alt-light);
-   padding: 10px 35px;
-   border-radius: 5px;
-   border: 2px solid var(--color-brand-blue);
-   box-shadow: 1px 1px 5px #aaa;
+  display: inline-block;
+  background: var(--uvalib-blue-alt-light);
+  padding: 10px 35px;
+  border-radius: 5px;
+  border: 2px solid var(--color-brand-blue);
+  box-shadow: 1px 1px 5px #aaa;
 }
 </style>
-
