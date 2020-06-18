cd18c60aba9ec8288fa53ebd1b968fab0a1638db
Lou Foster <lf6f@virginia.edu>
2020-06-18 10:37:58 -0400

    added some logging to trigger

--- frontend/src/analytics.js
+++ frontend/src/analytics.js
@@ -1,7 +1,9 @@
 export default {
    trigger: function(action, name, value) { 
       if (window._paq ) {
+         console.log(`Trigger trackEvent ${action} ${name} ${value}`)
          window._paq.push(['trackEvent', action, name, value])
+         console.log("trigger done")
       } else {
          console.error(`_PAQ is not available, cannot trigger ${action} ${name} ${value}`)
       }
