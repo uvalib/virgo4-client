d8d4f683d515e422b51af607543f2cccb577f89b
Jason Jordan <jlj5aj@virginia.edu>
2020-07-08 14:44:22 -0400

    VIRGONEW-1440: temporarily remove worldcat/jmrl pools

---
+++ backend/db/migrations/000031_remove_jmrl_worldcat_pools.down.sql
@@ -0,0 +1,3 @@
+INSERT into sources (private_url,name,public_url) VALUES 
+   ('http://pool-jmrl-ws-staging.private.staging:8080', 'jmrl', 'https://pool-jmrl-ws-dev.internal.lib.virginia.edu'),
+   ('http://pool-worldcat-ws-staging.private.staging:8080', 'worldcat', 'https://pool-worldcat-ws-dev.internal.lib.virginia.edu');
