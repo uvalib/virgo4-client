0ec52782bf909ec06b8bce53ec4ef8b0e0eb3ed5
Lou Foster <lf6f@virginia.edu>
2020-08-14 15:09:11 -0400

    fix layout when acceeURL has no provider info

--- frontend/src/components/AccessURLDetails.vue
+++ frontend/src/components/AccessURLDetails.vue
@@ -11,7 +11,7 @@
                </a>
             </template>
             <template v-else>
-               <div class="header" :class="{full: mode=='full'}">
+               <div v-if="hasProviderInfo" class="header" :class="{full: mode=='full'}">
                   <template v-if="providerHomepage(p.provider)">
                      <a :aria-label="`${providerLabel(p.provider)} home page`" :href="providerHomepage(p.provider)" target="_blank">
                         <img class="logo" v-if="mode=='full' && providerLogo(p.provider)" :src="providerLogo(p.provider)" />
@@ -19,10 +19,10 @@
                      </a>
                   </template>
                   <template v-else>
-                     <span class='provider'>{{ providerLabel(p.provider) }}</span>  
-                  </template>  
+                     <span class='provider'>{{ providerLabel(p.provider) }}</span>
+                  </template>
                </div>
-               <div class="links" :class="{full: mode=='full'}">
+               <div class="links" :class="{full: mode=='full', indent: hasProviderInfo}">
                   <template v-for="(l,idx) in providerLinks(p)">
                      <div :key="`${l.url}-${idx}`">
                         <a :href="l.url" target="_blank" :aria-label="`access ${title} ${l.label} with ${providerLabel(p.provider)}`">
@@ -67,29 +67,32 @@ export default {
       ...mapGetters({
          findProvider: 'pools/findProvider'
       }),
+      hasProviderInfo(provider) {
+         return this.findProvider(this.pool, provider) != null
+      },
    },
    methods: {
       providerLabel(provider) {
          let p = this.findProvider(this.pool, provider)
-         if ( p ) return p.label 
+         if ( p ) return p.label
          return provider
       },
       providerLogo(provider) {
          let p = this.findProvider(this.pool, provider)
-         if ( p ) return p.logo_url 
+         if ( p ) return p.logo_url
          return ""
       },
       providerHomepage(provider) {
          let p = this.findProvider(this.pool, provider)
-         if ( p ) return p.homepage_url 
+         if ( p ) return p.homepage_url
          return ""
       },
       providerLinks( details ) {
-         if ( this.mode == "full") return details.links 
+         if ( this.mode == "full") return details.links
          return details.links.slice(0,10)
       },
       remainingLinks( details ) {
-         if ( this.mode == "full") return 0   
+         if ( this.mode == "full") return 0
          if ( details.links.length <= 10) return 0
          return details.links.length - 10
       }
@@ -118,8 +121,10 @@ span.provider.link {
    color: var(--color-link);
    font-weight:  500 !important;
 }
-.links {
+.links.indent {
    margin: 0 0 0 20px;
+}
+.links {
    word-break: break-word;
    -webkit-hyphens: auto;
    -moz-hyphens: auto;
