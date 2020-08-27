db5a63bf5e5b011348161cd8d23401b489f2c43a
Lou Foster <lf6f@virginia.edu>
2020-08-27 14:57:50 -0400

    all non-provider links look like buttons

--- frontend/src/components/AccessURLDetails.vue
+++ frontend/src/components/AccessURLDetails.vue
@@ -25,7 +25,7 @@
                <div class="links" :class="{full: mode=='full', indent: hasProviderInfo}">
                   <template v-for="(l,idx) in providerLinks(p)">
                      <div :key="`${l.url}-${idx}`">
-                        <a :href="l.url" target="_blank" :aria-label="`access ${title} ${l.label} with ${providerLabel(p.provider)}`">
+                        <a class="link-button" :href="l.url" target="_blank" :aria-label="`access ${title} ${l.label} with ${providerLabel(p.provider)}`">
                            <template v-if="l.label">{{l.label}}</template>
                            <template v-else>{{l.url}}</template>
                         </a>
