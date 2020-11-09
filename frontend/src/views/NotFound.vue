74b056b9c745ce58883f5065423eafbdf45e829b
Lou Foster <lf6f@virginia.edu>
2020-11-09 11:15:24 -0500

    sparse not found page

--- frontend/src/views/NotFound.vue
+++ frontend/src/views/NotFound.vue
@@ -3,23 +3,6 @@
       <h1>Page Not Found</h1>
       <div class="info">
          <p>We're sorry, but we can't find that page.</p>
-         <p>You may wish to search Virgo or consult our other resources for finding research materials listed below.</p>
-      </div>
-      <div class="info">
-         <div class="subhead">Where Else Can I Find Research Materials?</div>
-         <div class="details">
-            <div class=note>
-               You can also explore the resources listed below to find research materials.
-               The Library's departmental guides, comprehensive database list, and journal finder tools can help you find
-               on-topic articles, datasets, and more.
-            </div>
-            <div class="break">Resources</div>
-            <ul class="links">
-               <li><a href="https://library.virginia.edu/research" target="_blank">Subject Guides</a></li>
-               <li><a href="https://guides.lib.virginia.edu/journalfinder" target="_blank">Journal Finders</a></li>
-               <li><a href="https://guides.lib.virginia.edu/az.php" target="_blank">Databases A–Z</a></li>
-            </ul>
-         </div>
       </div>
    </div>
 </template>
@@ -40,24 +23,9 @@ export default {
    margin: 25px 0;
    .info {
       width:50%;
-      margin: 5px auto 30px auto;
-      text-align: left;
-      font-size: 1.1em;
-   }
-   .break {
-      margin: 15px 0;
-      font-weight: bold;
-      border-bottom: 1px solid #ccc;
-      border-top: 1px solid #ccc;
-      padding: 3px;
-   }
-   .subhead {
-      margin: 0 0 10px 0;
-      font-weight: bold;
-   }
-   ul {
-      list-style: none;
-      padding-left: 0;
+      margin: 35px auto 30px auto;
+      text-align: center;
+      font-size: 1.4em;
    }
 }
 @media only screen and (min-width: $breakpoint-mobile) {
