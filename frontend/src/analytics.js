f591d547f49397724926f5bf178e0275f0b2e3d6
Nestor Walker <naw4t@virginia.edu>
2020-11-11 11:57:56 -0500

    Remove extra analytics console logging

--- frontend/src/analytics.js
+++ frontend/src/analytics.js
@@ -1,11 +1,11 @@
 export default {
    trigger: function(action, name, value) {
       if (window._paq ) {
-         console.log(`Trigger trackEvent ${action} ${name} ${value}`)
+         //console.log(`Trigger trackEvent ${action} ${name} ${value}`)
          window._paq.push(['trackEvent', action, name, value])
-         console.log("trigger done")
+         //console.log("trigger done")
       } else {
-         console.error(`_PAQ is not available, cannot trigger ${action} ${name} ${value}`)
+         //console.error(`_PAQ is not available, cannot trigger ${action} ${name} ${value}`)
       }
    }
  }
