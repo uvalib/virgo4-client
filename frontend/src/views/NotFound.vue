68b93ffc946b34d00d4913ba080c86cf9d90fd1a
Lou Foster <lf6f@virginia.edu>
2020-12-08 10:09:03 -0500

    VIRGONEW-1942 update 404 page content. fixed probles with session maintenance; simplified code

--- frontend/src/views/NotFound.vue
+++ frontend/src/views/NotFound.vue
@@ -2,7 +2,20 @@
    <div class="not-found">
       <h1>Page Not Found</h1>
       <div class="info">
-         <p>We're sorry, but we can't find that page.</p>
+         <p>
+            It looks like there’s a typo in this address or perhaps an outdated link led you to the wrong place.
+            Don't worry – there are lots of places to go from here:
+         </p>
+         <ul>
+            <li>Click "<router-link to="/search">Search</router-link>" above to begin a new search in the Library’s catalog.</li>
+            <li>To access <router-link to="/account">account information</router-link>, click "Sign in" above. If you are already signed in, it will read "Signed in as..."" with your ID.</li>
+            <li>
+               Use the "Library Services" link above or visit the
+               <a href="http://library.virginia.edu/" target="_blank">Library home page</a>
+               to see more options for Library services.
+            </li>
+            <li>You’re always welcome to <a href="http://www.library.virginia.edu/askalibrarian/" target="_blank">Ask a Librarian</a> for help!</li>
+         </ul>
       </div>
    </div>
 </template>
@@ -25,7 +38,11 @@ export default {
       width:50%;
       margin: 35px auto 30px auto;
       text-align: center;
-      font-size: 1.4em;
+      font-size: 1.25em;
+   }
+   ul {
+      font-size: 0.9em;
+      text-align: left;
    }
 }
 @media only screen and (min-width: $breakpoint-mobile) {
