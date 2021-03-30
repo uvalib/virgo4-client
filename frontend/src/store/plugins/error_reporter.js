c68be05b02fa785380007310088798e0139a55f6
Lou Foster <lf6f@virginia.edu>
2021-03-30 14:50:20 -0400

    dont report net/connection errors

--- frontend/src/store/plugins/error_reporter.js
+++ frontend/src/store/plugins/error_reporter.js
@@ -4,9 +4,14 @@ const errorReporter = store => {
          if (mutation.payload != "" && mutation.payload != {}) {
             let err = mutation.payload
             if ( typeof err != "string" ) {
-               // it is ok to report all non-string errors
-               store.dispatch("system/reportError", err)
-            } else if (err.includes("System error, we regret the inconvenience") == false ) {
+               // it is ok to report all non-string errors (except net connectivity problems)
+               if (err.toString().includes("Network Error") == false || err.toString().includes("ECONNREFUSED") == false) {
+                  store.dispatch("system/reportError", err)
+               }
+            } else if (err.includes("System error, we regret the inconvenience") == false &&
+                       err.includes("Network Error") == false &&
+                       err.includes("ECONNREFUSED") == false &&
+                       err.includes("Internal Server Error") == false ) {
                // don't report the generic error. The actual error will be reported when it is caught (searchAll/One in index.js)
                store.dispatch("system/reportError", err)
             }
