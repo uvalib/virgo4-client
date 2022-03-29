357eea2fc67e6f354bb9bd99b052824baa751066
Lou Foster <louffoster@gmail.com>
2022-03-29 09:53:50 -0400

    openurl forms converted

--- frontend/src/components/requests/openurl/Book.vue
+++ frontend/src/components/requests/openurl/Book.vue
@@ -3,61 +3,65 @@
       <div class="request-content pure-form">
          <div class="entry pure-control-group">
             <label for="title">Title<span class="required">*</span></label>
-            <input type="text" v-model="title" id="title" aria-required="true" required="required">
+            <input type="text" v-model="request.openurl.title" id="title" aria-required="true" required="required">
             <span class="note">Please do not abbreviate title</span>
             <span v-if="hasError('title')" class="error">Title is required</span>
          </div>
          <div class="entry pure-control-group">
             <label for="author">Author/Editor</label>
-            <input type="text" v-model="author" id="author">
+            <input type="text" v-model="request.openurl.author" id="author">
          </div>
          <div class="entry pure-control-group">
             <label for="publisher">Publisher</label>
-            <input type="text" v-model="publisher" id="publisher">
+            <input type="text" v-model="request.openurl.publisher" id="publisher">
          </div>
          <div class="entry pure-control-group">
             <label for="volume">Volume</label>
-            <input type="text" v-model="volume" id="volume">
+            <input type="text" v-model="request.openurl.volume" id="volume">
          </div>
          <div class="entry pure-control-group">
             <label for="year">Year<span class="required">*</span></label>
-            <input type="text" v-model="year" id="year" aria-required="true" required="required">
+            <input type="text" v-model="request.openurl.year" id="year" aria-required="true" required="required">
             <span v-if="hasError('year')" class="error">Year is required</span>
          </div>
          <div class="entry pure-control-group">
             <label for="edition">Edition</label>
-            <input type="text" v-model="edition" id="edition">
+            <input type="text" v-model="request.openurl.edition" id="edition">
          </div>
          <div class="entry pure-control-group">
             <label for="issn">ISSN/ISBN</label>
-            <input type="text" v-model="issn" id="issn">
+            <input type="text" v-model="request.openurl.issn" id="issn">
          </div>
          <div class="entry pure-control-group">
             <label for="oclc">OCLC Number</label>
-            <input type="text" v-model="oclc" id="oclc">
+            <input type="text" v-model="request.openurl.oclc" id="oclc">
          </div>
          <div class="entry pure-control-group">
             <label for="bydate">Need By Date<span class="required">*</span></label>
-            <input type="text" v-model="bydate" id="bydate">
+            <input type="text" v-model="request.openurl.bydate" id="bydate">
             <span v-if="hasError('bydate')" class="error">Need By Date is required</span>
          </div>
          <div class="entry pure-control-group">
             <label for="citedin">Cited In</label>
-            <textarea id="citedin" v-model="citedin"></textarea>
+            <textarea id="citedin" v-model="request.openurl.citedin"></textarea>
          </div>
          <div role="radiogroup" class="entry pure-control-group" aria-labelledby="language-label">
             <label id="language-label">
                Will you accept the item in a language other than English?
             </label>
-            <V4Button id="any-language-yes" class="radio" mode="icon" @click="anylanguage='true'" role="radio"
-               :aria-checked="(anylanguage=='true').toString()">
-               <i v-if="anylanguage=='true'" class="check fas fa-check-circle"></i>
+            <V4Button id="any-language-yes" class="radio" mode="icon"
+               @click="request.openurl.anylanguage='true'"
+               role="radio"
+               :aria-checked="(request.openurl.anylanguage=='true').toString()">
+               <i v-if="request.openurl.anylanguage=='true'" class="check fas fa-check-circle"></i>
                <i v-else class="check far fa-circle"></i>
                Yes
             </V4Button>
-            <V4Button class="radio" mode="icon" @click="anylanguage='false'" role="radio"
-               :aria-checked="(anylanguage=='false').toString()">
-               <i v-if="anylanguage=='false'" class="check fas fa-check-circle"></i>
+            <V4Button class="radio" mode="icon"
+               @click="request.openurl.anylanguage='false'"
+               role="radio"
+               :aria-checked="(request.openurl.anylanguage=='false').toString()">
+               <i v-if="request.openurl.anylanguage=='false'" class="check fas fa-check-circle"></i>
                <i v-else class="check far fa-circle"></i>
                No
             </V4Button>
@@ -65,97 +69,72 @@
          </div>
          <div class="entry pure-control-group">
             <label for="notes">Notes or Special Instructions</label>
-            <textarea id="notes" v-model="notes"></textarea>
+            <textarea id="notes" v-model="request.openurl.notes"></textarea>
             <span class="note">(ex: special edition)</span>
          </div>
          <div class="entry pure-control-group">
             <label for="doctype">Preferred pickup location</label>
-            <select v-model="pickup" id="pickup">
+            <select v-model="request.openurl.pickup" id="pickup">
                <option value="">Select a location</option>
-               <option v-for="l in pickupLibraries" :key="l.id" :value="l.id">{{l.name}}</option>
+               <option v-for="l in user.libraries" :key="l.id" :value="l.id">{{l.name}}</option>
             </select>
          </div>
       </div>
       <div class="controls">
-         <V4Button mode="tertiary" @click="$emit('canceled')">
+         <V4Button mode="tertiary" @click="emit('canceled')">
             Cancel
          </V4Button>
-         <V4Button mode="primary" @click="submitClicked" :disabled="buttonDisabled">
+         <V4Button mode="primary" @click="submitClicked" :disabled="request.buttonDisabled">
             Submit
          </V4Button>
       </div>
    </div>
 </template>
-<script>
-import { mapFields } from "vuex-map-fields"
-import { mapState } from "vuex"
-import { mapGetters } from "vuex"
-export default {
-   data: () => {
-      return {
-         errors: [],
-         required: ['title', 'year', 'bydate']
+
+<script setup>
+import { ref, onMounted } from 'vue'
+import { useRequestStore } from "@/stores/request"
+import { useUserStore } from "@/stores/user"
+import { usePreferencesStore } from "@/stores/preferences"
+
+const emit = defineEmits( ['submitted', 'canceled'] )
+
+const request = useRequestStore()
+const user = useUserStore()
+const preferences = usePreferencesStore()
+
+const required = ['title', 'year', 'bydate']
+const errors = ref([])
+
+onMounted(()=>{
+   setTimeout(() => {
+      request.openurl.pickup = preferences.pickupLibrary
+      document.getElementById("title").focus()
+   }, 150)
+})
+
+function hasError(val) {
+   return errors.value.includes(val)
+}
+function submitClicked() {
+   errors.value.splice(0, errors.value.length)
+   for (let [key, value] of Object.entries(request.openurl)) {
+      if ( required.includes(key) && value == "") {
+         errors.value.push(key)
       }
-   },
-   computed: {
-      ...mapState({
-         buttonDisabled: state => state.requests.buttonDisabled,
-         request: state => state.requests.openurl,
-         preferredPickupLibrary: state => state.preferences.pickupLibrary
-      }),
-      ...mapGetters({
-         pickupLibraries: "user/libraries",
-      }),
-      ...mapFields('requests',[
-         'buttonDisabled',
-         'openurl.title',
-         'openurl.article',
-         'openurl.author',
-         'openurl.publisher',
-         'openurl.volume',
-         'openurl.year',
-         'openurl.edition',
-         'openurl.issn',
-         'openurl.oclc',
-         'openurl.bydate',
-         'openurl.citedin',
-         'openurl.anylanguage',
-         'openurl.notes',
-         'openurl.pickup',
-      ]),
-   },
-   created() {
-      setTimeout(() => {
-         this.pickup = this.preferredPickupLibrary.id
-         document.getElementById("title").focus()
-      }, 150);
-   },
-   methods: {
-      hasError(val) {
-         return this.errors.includes(val)
-      },
-      submitClicked() {
-         this.errors.splice(0, this.errors.length)
-         for (let [key, value] of Object.entries(this.request)) {
-            if ( this.required.includes(key) && value == "") {
-               this.errors.push(key)
-            }
-         }
-
-         if (this.errors.length > 0) {
-            console.error(this.errors)
-            let tgtID = this.errors[0]
-            let first = document.getElementById(tgtID)
-            if ( first ) {
-               first.focus()
-            }
-         } else {
-            this.$emit("submitted")
-         }
-
-      },
    }
-};
+
+   if (errors.value.length > 0) {
+      console.error(errors.value)
+      let tgtID = errors.value[0]
+      let first = document.getElementById(tgtID)
+      if ( first ) {
+         first.focus()
+      }
+   } else {
+      emit("submitted")
+   }
+}
 </script>
 <style lang="scss" scoped>
 .request-content {
--- frontend/src/components/requests/openurl/BookChapter.vue
+++ frontend/src/components/requests/openurl/BookChapter.vue
@@ -3,131 +3,110 @@
       <div class="request-content pure-form">
          <div class="entry pure-control-group">
             <label for="title">Book Title<span class="required">*</span></label>
-            <input type="text" v-model="title" id="title" aria-required="true" required="required">
+            <input type="text" v-model="request.openurl.title" id="title" aria-required="true" required="required">
             <span v-if="hasError('title')" class="error">Book Title is required</span>
          </div>
          <div class="entry pure-control-group">
             <label for="article">Chapter Title<span class="required">*</span></label>
-            <input type="text" v-model="article" id="article" aria-required="true" required="required">
+            <input type="text" v-model="request.openurl.article" id="article" aria-required="true" required="required">
             <span v-if="hasError('article')" class="error">Chapter Title is required</span>
          </div>
          <div class="entry pure-control-group">
             <label for="author">Book or Chapter Author</label>
-            <input type="text" v-model="author" id="author">
+            <input type="text" v-model="request.openurl.author" id="author">
          </div>
          <div class="entry pure-control-group">
             <label for="year">Year<span class="required">*</span></label>
-            <input type="text" v-model="year" id="year" aria-required="true" required="required">
+            <input type="text" v-model="request.openurl.year" id="year" aria-required="true" required="required">
             <span v-if="hasError('year')" class="error">Year is required</span>
          </div>
          <div class="entry pure-control-group">
             <label for="pages">Pages<span class="required">*</span></label>
-            <input type="text" v-model="pages" id="pages">
+            <input type="text" v-model="request.openurl.pages" id="pages">
             <span class="note">(ex: 1-15)</span>
             <span v-if="pageLengthError" class="error">Please limt page information to 25 characters</span>
              <span v-if="hasError('pages')" class="error">Pages are required</span>
          </div>
          <div class="entry pure-control-group">
             <label for="issn">ISSN/ISBN</label>
-            <input type="text" v-model="issn" id="issn">
+            <input type="text" v-model="request.openurl.issn" id="issn">
          </div>
          <div class="entry pure-control-group">
             <label for="oclc">OCLC Number</label>
-            <input type="text" v-model="oclc" id="oclc">
+            <input type="text" v-model="request.openurl.oclc" id="oclc">
          </div>
          <div class="entry pure-control-group">
             <label for="notes">Notes or Special Instructions</label>
-            <textarea id="notes" v-model="notes"></textarea>
+            <textarea id="notes" v-model="request.openurl.notes"></textarea>
             <span class="note">(ex: color copies)</span>
          </div>
          <div class="entry pure-control-group">
             <label for="bydate">Need By Date<span class="required">*</span></label>
-            <input type="text" v-model="bydate" id="bydate">
+            <input type="text" v-model="request.openurl.bydate" id="bydate">
             <span v-if="hasError('bydate')" class="error">Need By Date is required</span>
          </div>
       </div>
       <div class="controls">
-         <V4Button mode="tertiary" @click="$emit('canceled')">
+         <V4Button mode="tertiary" @click="emit('canceled')">
             Cancel
          </V4Button>
-         <V4Button mode="primary" @click="submitClicked" :disabled="buttonDisabled">
+         <V4Button mode="primary" @click="submitClicked" :disabled="request.buttonDisabled">
             Submit
          </V4Button>
       </div>
    </div>
 </template>
-<script>
-import { mapFields } from "vuex-map-fields"
-import { mapState } from "vuex"
-import { mapGetters } from "vuex"
-export default {
-   data: () => {
-      return {
-         errors: [],
-         pageLengthError: false,
-         required: ['title', 'article', 'year', 'pages', 'bydate']
+
+<script setup>
+import { ref, onMounted } from 'vue'
+import { useRequestStore } from "@/stores/request"
+
+const emit = defineEmits( ['submitted', 'canceled'] )
+
+const request = useRequestStore()
+
+const required = ['title', 'article', 'year', 'pages', 'bydate']
+const errors = ref([])
+const pageLengthError = ref(false)
+
+onMounted(()=>{
+   setTimeout(() => {
+      document.getElementById("title").focus()
+   }, 150)
+})
+
+
+function hasError(val) {
+   return errors.value.includes(val)
+}
+function submitClicked() {
+   errors.value.splice(0, errors.value.length)
+   for (let [key, value] of Object.entries(request.openurl)) {
+      if ( required.includes(key) && value == "") {
+         errors.value.push(key)
       }
-   },
-   computed: {
-      ...mapState({
-         buttonDisabled: state => state.requests.buttonDisabled,
-         request: state => state.requests.openurl,
-      }),
-      ...mapGetters({
-         pickupLibraries: "user/libraries",
-      }),
-      ...mapFields('requests',[
-         'buttonDisabled',
-         'openurl.title',
-         'openurl.article',
-         'openurl.author',
-         'openurl.year',
-         'openurl.pages',
-         'openurl.issn',
-         'openurl.oclc',
-         'openurl.bydate',
-         'openurl.notes',
-      ]),
-   },
-   created() {
-      setTimeout(() => {
-         document.getElementById("title").focus()
-      }, 150);
-   },
-   methods: {
-      hasError(val) {
-         return this.errors.includes(val)
-      },
-      submitClicked() {
-         this.errors.splice(0, this.errors.length)
-         for (let [key, value] of Object.entries(this.request)) {
-            if ( this.required.includes(key) && value == "") {
-               this.errors.push(key)
-            }
-         }
-
-         if (this.errors.length > 0) {
-            let tgtID = this.errors[0]
-            let first = document.getElementById(tgtID)
-            if ( first ) {
-               first.focus()
-            }
-         } else {
-            this.pageLengthError =  (this.pages.length > 25)
-            if ( this.pageLengthError) {
-               let tgtID = `pages`
-               let first = document.getElementById(tgtID)
-               if ( first ) {
-                  first.focus()
-               }
-            } else {
-               this.$emit("submitted")
-            }
-         }
-
-      },
    }
-};
+
+   if (errors.value.length > 0) {
+      let tgtID = errors.value[0]
+      let first = document.getElementById(tgtID)
+      if ( first ) {
+         first.focus()
+      }
+   } else {
+      pageLengthError.value =  (request.openurl.pages.length > 25)
+      if ( pageLengthError.value) {
+         let tgtID = `pages`
+         let first = document.getElementById(tgtID)
+         if ( first ) {
+            first.focus()
+         }
+      } else {
+         emit("submitted")
+      }
+   }
+
+}
 </script>
 <style lang="scss" scoped>
 .request-content {
