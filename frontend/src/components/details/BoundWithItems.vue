638ed1ea9e2c80220f696fdeaebabc406566c1d2
Lou Foster <louffoster@gmail.com>
2022-03-29 11:04:15 -0400

    bound with converted to pinia/composition api

--- frontend/src/components/details/BoundWithItems.vue
+++ frontend/src/components/details/BoundWithItems.vue
@@ -3,38 +3,31 @@
 
   <p class="group-header">Bound in:</p>
   <div class="items">
-    <router-link class="card" v-for="item in boundIn" :key="item.itemID" :to="'/items/u'+item.title_id">
-      <p class="title">{{item.title}}</p>
-      <p>{{item.author}}</p>
-      <p>{{item.call_number}}</p>
-      <p v-if="'u'+item.title_id != identifier">(Click for availability)</p>
+    <router-link class="card" v-for="boundItem in item.boundIn" :key="boundItem.itemID" :to="'/items/u'+boundItem.title_id">
+      <p class="title">{{boundItem.title}}</p>
+      <p>{{boundItem.author}}</p>
+      <p>{{boundItem.call_number}}</p>
+      <p v-if="'u'+boundItem.title_id != item.identifier">(Click for availability)</p>
     </router-link>
   </div>
 
   <p class="group-header">Bound with these items:</p>
   <div class="items">
-    <a class="card" v-for="item in boundWith" :key="item.title_id" :href="'/items/u'+item.title_id">
-      <p class="title">{{item.title}}</p>
-      <p>{{item.author}}</p>
-      <p>{{item.call_number}}</p>
+    <a class="card" v-for="boundItem in item.boundWith" :key="boundItem.title_id" :href="'/items/u'+boundItem.title_id">
+      <p class="title">{{boundItem.title}}</p>
+      <p>{{boundItem.author}}</p>
+      <p>{{boundItem.call_number}}</p>
     </a>
 
   </div>
 </div>
 </template>
-<script>
-import { mapGetters } from "vuex"
-export default {
-  computed: {
-      ...mapGetters({
-        boundIn: 'item/boundIn',
-        boundWith: 'item/boundWith',
-        identifier: 'item/identifier'
-      }),
-  },
 
-}
+<script setup>
+import { useItemStore } from "@/stores/item"
+const item = useItemStore()
 </script>
+
 <style lang="scss" scoped>
 .bound-with {
   border: 1px solid var(--uvalib-grey-light);
