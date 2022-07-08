e9c423c88c9dff7a83aa0a2ff849dfa1c6052e17
Nestor Walker <naw4t@virginia.edu>
2022-07-08 10:54:57 -0400

    DiBS iframe size

--- frontend/src/components/details/DiBSViewer.vue
+++ frontend/src/components/details/DiBSViewer.vue
@@ -1,6 +1,10 @@
 <template>
  <div class="dibs">
-  <iframe sandbox="allow-same-origin allow-scripts allow-forms" src="https://reserves-dev.library.virginia.edu/item/X001492667"></iframe>
+  <iframe sandbox="allow-same-origin allow-scripts allow-forms"
+  src="https://reserves-dev.library.virginia.edu/item/X001492667"
+  width="100%"
+  height="600"
+  ></iframe>
  </div>
 </template>
 <script >
