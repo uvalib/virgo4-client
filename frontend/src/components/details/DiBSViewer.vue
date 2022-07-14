9c8b8b74a5e81b0b79ce2e8d8b7c281f53045ce3
Nestor Walker <naw4t@virginia.edu>
2022-07-14 12:20:50 -0400

    Require sign in for DIBS

--- frontend/src/components/details/DiBSViewer.vue
+++ frontend/src/components/details/DiBSViewer.vue
@@ -1,13 +1,20 @@
 <template>
- <div class="dibs">
-  <iframe
-  :src="'https://reserves-dev.library.virginia.edu/item/' + props.barcode"
-  width="100%"
-  height="600"
-  ></iframe>
- </div>
+   <div class="dibs">
+      <template v-if="userStore.isSignedIn">
+         <iframe :src="'https://reserves-dev.library.virginia.edu/item/' + props.barcode"></iframe>
+      </template>
+      <SignInRequired v-else targetPage="electronic reserves">
+
+
+      </SignInRequired>
+   </div>
 </template>
 <script setup>
+import { useUserStore } from "@/stores/user"
+import SignInRequired from "../account/SignInRequired.vue";
+
+const userStore = useUserStore()
+
 const props = defineProps({
    barcode: {
       type: String,
@@ -15,5 +22,11 @@ const props = defineProps({
    }
 })
 </script>
-<style >
+<style lang="scss" scoped>
+.dibs {
+   iframe {
+      width: 100%;
+      height: 80vh;
+   }
+}
 </style>
