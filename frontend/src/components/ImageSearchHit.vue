b77ecf0e7a8ca408dec880ed0247da3922dac08a
Lou Foster <louffoster@gmail.com>
2024-03-13 15:33:53 -0400

    if content_advisory attribute present dim image search hit and show a message

--- frontend/src/components/ImageSearchHit.vue
+++ frontend/src/components/ImageSearchHit.vue
@@ -8,7 +8,15 @@
          </span>
       </div>
       <router-link @mousedown="detailClicked" class="img-link" :to="detailsURL">
-         <img aria-label=" " :src="iiifURL(props.hit)">
+         <div class="wrap">
+            <img aria-label=" " :src="iiifURL(props.hit)">
+            <div class="advisory" v-if="hasContentAdvisory">
+               <div class="message">
+                  <span class="icon"></span>
+                  <span>Content Advisory</span>
+               </div>
+            </div>
+         </div>
          <div class="metadata-content">
             <div>{{props.hit.header.title}}</div>
          </div>
@@ -31,6 +39,11 @@ const resultStore = useResultStore()
 
 const detailsURL = computed(()=>{
    return `/sources/${props.pool}/items/${props.hit.identifier}`
+})
+const hasContentAdvisory = computed(() => {
+   let allFields = [...props.hit.basicFields.concat(props.hit.detailFields)]
+   let idx = allFields.findIndex( f=> f.name=="content_advisory")
+   return idx > -1
 })
 
 function detailClicked() {
@@ -77,6 +90,47 @@ function iiifURL(item) {
 
       .buttons {
          margin-left: auto;
+      }
+   }
+
+   .wrap {
+      position: relative;
+      .advisory {
+         position: absolute;
+         top:0;
+         left:0;
+         width:100%;
+         height: 100%;
+         background-color: #2B2B2B;
+         opacity: 0.9;
+         .message {
+            position: absolute;
+            top: 50%;
+            left: 0;
+            transform: translate(0, -50%);
+            width: 100%;
+            span.icon {
+               display: block;
+               width: 34px;
+               height: 34px;
+               color: white;
+               background-image: url('@/assets/eye-slash-white.svg');
+               background-repeat:no-repeat;
+               background-position: center center;
+            }
+            span {
+               font-family: "franklin-gothic-urw", arial, sans-serif;
+               -webkit-font-smoothing: antialiased;
+               -moz-osx-font-smoothing: grayscale;
+               padding: 0;
+               color: white;
+               font-size: 17px;
+               font-weight: 500;
+               display: block;
+               margin: 10px auto;
+               text-align: center;
+            }
+         }
       }
    }
 
