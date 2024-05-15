982f20257e7d0c8609e3500e7fd88997774a45bc
Lou Foster <louffoster@gmail.com>
2024-05-15 11:11:18 -0400

    roll back zotero test

--- frontend/src/views/Details.vue
+++ frontend/src/views/Details.vue
@@ -15,7 +15,7 @@
 import ItemView from "@/components/details/ItemView.vue"
 import CollectionHeader from "@/components/details/CollectionHeader.vue"
 import FullPageCollectionView from "@/components/details/FullPageCollectionView.vue"
-import { onMounted, watch } from 'vue'
+import { onMounted, onUpdated, watch } from 'vue'
 import { useRoute, onBeforeRouteUpdate } from 'vue-router'
 import { useItemStore } from "@/stores/item"
 import { useResultStore } from "@/stores/result"
@@ -93,8 +93,6 @@ async function getDetails(src, id) {
       }
    }
 
-   zoteroItemUpdated()
-
    setTimeout( () => {
       if ( restore.pendingBookmark && (restore.pendingBookmark.origin == "DETAIL" || restore.pendingBookmark.origin == "COLLECTION") ) {
          let newBM = restore.pendingBookmark
@@ -121,6 +119,9 @@ onMounted(()=>{
    getDetails(route.params.src, route.params.id)
 })
 
+onUpdated(()=>{
+   zoteroItemUpdated()
+})
 </script>
 <style lang="scss" scoped>
 .details {
