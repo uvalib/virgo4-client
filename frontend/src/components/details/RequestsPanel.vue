9dafc57ec3c2a604b3819a6c65f0bf85abce4540
Lou Foster <louffoster@gmail.com>
2024-08-29 08:34:48 -0400

    no panel around request buttons, siack with max witdth on mobile

--- frontend/src/components/details/RequestsPanel.vue
+++ frontend/src/components/details/RequestsPanel.vue
@@ -1,13 +1,8 @@
 <template>
    <section class="requests" aria-live="polite">
-      <div class="panel">
-         <div class="gutter"></div>
-         <div class="buttons">
-            <PlaceHoldDialog v-if="request.hasRequestOption('hold')" :settings="request.requestOption('hold')" :show="request.activePanel=='hold'"/>
-            <VirgoButton v-if="request.hasRequestOption('scan')" label="Request a scan" size="small" />
-            <VirgoButton v-if="request.hasRequestOption('aeon')" label="Request in Special Colections" size="small" />
-         </div>
-      </div>
+      <PlaceHoldDialog v-if="request.hasRequestOption('hold')" :settings="request.requestOption('hold')" :show="request.activePanel=='hold'"/>
+      <VirgoButton v-if="request.hasRequestOption('scan')" label="Request a scan" size="small" />
+      <VirgoButton v-if="request.hasRequestOption('aeon')" label="Request in Special Colections" size="small" />
    </section>
 </template>
 
@@ -20,39 +15,27 @@ const request = useRequestStore()
 </script>
 
 <style lang="scss" scoped>
-.requests {
-   .panel {
+   .requests {
+      flex: 1;
       display: flex;
-      flex-flow: row nowrap;
-      justify-content: flex-start;
-      align-items: stretch;
-      border: 1px solid var(--uvalib-grey-light);
-      border-radius: 0 5px 5px 0;
-      gap: 20px;
-      margin-bottom:30px;
-      .gutter {
-         flex: 0 0 17px;
-         background-color:#E6F2F7;
-      }
-      .buttons {
-         flex: 1;
-         display: flex;
-         padding: 20px 20px 20px 0;
-      }
+      padding-bottom: 20px;
    }
-}
+
 @media only screen and (min-width: 768px) {
-   .buttons {
+   .requests {
       flex-flow: row wrap;
       align-items: flex-start;
       gap: 10px;
    }
 }
 @media only screen and (max-width: 768px) {
-   .buttons {
+   .requests {
       flex-direction: column;
       align-items: stretch;
       gap: 5px;
+      :deep(button.p-button) {
+         max-width:350px !important;
+      }
    }
 }
 </style>
