6bf180636a1d3fec29d922b41cd0a9ba78e8ea25
Lou Foster <louffoster@gmail.com>
2024-11-14 16:22:11 -0500

    add vueUse scroll watching to simplify scroll to top. make style match main library

--- frontend/src/components/ScrollToTop.vue
+++ frontend/src/components/ScrollToTop.vue
@@ -2,93 +2,59 @@
    <transition name="message-transition"
          enter-active-class="animated faster fadeIn"
          leave-active-class="animated faster fadeOut">
-      <div v-if="showScrollTop && systemStore.hideScrollToTop == false" role="button"
-         @click.stop="backToTop" tab-index="0" aria-label="scroll to top of page"
-         class="scroll-to-top" :class="{mobile: smallScreen}"
+      <button v-if="smallScreen == false && showScrollTop && systemStore.hideScrollToTop == false"
+         @click.stop="backToTop" class="scroll-to-top"
       >
-         <i class="fal fa-angle-up"></i>
-      </div>
+         Top
+      </button>
    </transition>
 </template>
 
 <script setup>
 import { useSystemStore } from "@/stores/system"
-import { ref, computed, onMounted, onUnmounted } from 'vue'
+import { ref, computed, watch } from 'vue'
 import { useWindowSize } from '@vueuse/core'
+import { useWindowScroll } from '@vueuse/core'
 
 const { width } = useWindowSize()
+const { y } = useWindowScroll()
 const systemStore = useSystemStore()
 const showScrollTop = ref(false)
+
+watch(y, (newY) => {
+   if ( newY > 300 ) {
+      showScrollTop.value = true
+   } else {
+      showScrollTop.value = false
+   }
+})
 
 const smallScreen = computed( () => {
    return width.value <= 810
 })
 
-function backToTop() {
-   var scrollStep = -window.scrollY / (500 / 10),
-   scrollInterval = setInterval(()=> {
-      if ( window.scrollY != 0 ) {
-         window.scrollBy( 0, scrollStep )
-      } else {
-         clearInterval(scrollInterval)
-      }
-   },10)
-}
-function scrollChecker() {
-   if (window.window.scrollY > 150) {
-      showScrollTop.value = true
-   } else {
-      showScrollTop.value = false
-   }
-}
-
-onMounted(() => {
-   window.addEventListener("scroll", scrollChecker)
-}),
-onUnmounted(() => {
-   window.removeEventListener("scroll", scrollChecker)
+const backToTop = (()=> {
+   window.scrollTo({ top: 0, behavior: 'smooth' })
 })
 </script>
 
 <style scoped lang="scss">
 .scroll-to-top {
-   display: flex;
-   flex-basis: auto;
-   flex-direction: column;
    position: fixed;
-   background-color: white;
-   color: $uva-brand-orange;
-   font-size: 2.9em;
-   font-weight: 100;
-   border: 2px solid $uva-brand-orange;
-   border-radius: 50%;
+   bottom: 20px;
+   right: 30px;
+   z-index: 99;
+   outline: 2px solid #fff;
+   background-color: $uva-brand-blue;
+   color: #fff;
    cursor: pointer;
-   align-items: center;
-   bottom: 30px;
-   right: 25px;
-   box-shadow: rgba(0, 0, 0, 0.14) 0px 2px 2px 0px;
-   box-sizing: border-box;
-   justify-content: center;
-   min-width: 0px;
-   padding: 0px 0px 4px 0px;
-   width: 56px;
-   height: 56px;
-   z-index: 9999;
+   padding: 1.25rem 1rem;
+   border-radius: 50%;
+   font-size: 18px;
+   font-weight: 400;
    &:hover {
       color: white;
-      background-color: $uva-brand-orange;
+      background-color: $uva-grey-A;
    }
-}
-.scroll-to-top.mobile {
-   position: fixed;
-   background-color: white;
-   color: $uva-brand-orange;
-   border: 3px solid $uva-brand-orange;
-   font-size: 2em;
-   font-weight: 100;
-   padding: 0px 12px;
-   right: 5px;
-   bottom: 5px;
-   cursor: pointer;
 }
 </style>
