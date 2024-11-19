322ad4c9b60ccf280e97ca1682a4ec2637c19b77
Lou Foster <louffoster@gmail.com>
2024-11-19 11:44:12 -0500

    fix pinnable to handle window resize. dep updates

--- frontend/src/composables/pin.js
+++ frontend/src/composables/pin.js
@@ -1,5 +1,5 @@
-import { onMounted, ref, watch } from 'vue'
-import { useWindowScroll, useElementBounding } from '@vueuse/core'
+import { onMounted, ref, watch, nextTick } from 'vue'
+import { useWindowScroll, useElementBounding, useResizeObserver } from '@vueuse/core'
 
 export function usePinnable( pinID, alertsID, scrollID, footerID ) {
    const { y } = useWindowScroll()
@@ -10,33 +10,49 @@ export function usePinnable( pinID, alertsID, scrollID, footerID ) {
    const toolbarBounds = ref()
    const pinnedY = ref(-1)
 
+   useResizeObserver(footer, () => {
+      if ( pinnedY.value > -1) {
+         unpin()
+         nextTick( () => pin() )
+      }
+   })
+
    watch(y, (newY) => {
       if ( pinnedY.value < 0) {
          if ( toolbarBounds.value.top <= 0 ) {
             pinnedY.value = y.value+toolbarBounds.value.top
-            toolbar.value.classList.add("sticky")
-            toolbar.value.style.width = `${toolbarBounds.value.width}px`
-            main.value.style.top = `${toolbarBounds.value.height}px`
-            if ( alerts.value ) {
-               alerts.value.style.top = `${toolbarBounds.value.height}px`
-            }
-            footer.value.style.top = `${toolbarBounds.value.height}px`
+            pin()
          }
       } else {
          if ( newY <=  pinnedY.value) {
             pinnedY.value = -1
-            toolbar.value.classList.remove("sticky")
-            toolbar.value.style.width = `auto`
-            if ( alerts.value ) {
-               alerts.value.style.top = `auto`
-            }
-            main.value.style.top = `auto`
-            footer.value.style.top = `auto`
+            unpin()
          }
       }
    })
 
+   const unpin = ( () => {
+      toolbar.value.classList.remove("sticky")
+      toolbar.value.style.width = `auto`
+      if ( alerts.value ) {
+         alerts.value.style.top = `auto`
+      }
+      main.value.style.top = `auto`
+      footer.value.style.top = `auto`
+   })
+
+   const pin = (() => {
+      toolbar.value.classList.add("sticky")
+      toolbar.value.style.width = `${toolbarBounds.value.width}px`
+      main.value.style.top = `${toolbarBounds.value.height}px`
+      if ( alerts.value ) {
+         alerts.value.style.top = `${toolbarBounds.value.height}px`
+      }
+      footer.value.style.top = `${toolbarBounds.value.height}px`
+   })
+
    onMounted( () => {
+      console.log(document.getElementById(scrollID))
       alerts.value = document.getElementById(alertsID)
       main.value = document.getElementById(scrollID)
       footer.value = document.getElementById(footerID)
