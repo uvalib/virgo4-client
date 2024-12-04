ee9daca127c4266b90a179e50fbacf126aecb9ec
Lou Foster <louffoster@gmail.com>
2024-12-04 16:33:30 -0500

    break long urls on a button

--- frontend/src/components/details/OnlineAccessPanel.vue
+++ frontend/src/components/details/OnlineAccessPanel.vue
@@ -115,6 +115,14 @@ const linkLabel = (( providerLink ) => {
                gap: 10px;
                .p-button {
                   flex-grow: 1;
+                  overflow-wrap: break-word;
+                  word-wrap: break-word;
+                  -ms-word-break: break-all;
+                  word-break: break-word;
+                  -ms-hyphens: auto;
+                  -moz-hyphens: auto;
+                  -webkit-hyphens: auto;
+                  hyphens: auto;
                }
             }
          }
