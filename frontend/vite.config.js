dea14824b3a3f8e290fcde9bf6307263ce8b0e0e
Doug Chestnut <dougchestnut@gmail.com>
2026-01-10 16:01:04 -0500

    tweaks to get this running on my laptop

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,4 +1,4 @@
- /*global process */
+/*global process */
 
 import { fileURLToPath, URL } from 'url'
 import { defineConfig } from 'vite'
@@ -7,18 +7,20 @@ import vue from '@vitejs/plugin-vue'
 const hash = Math.floor(Math.random() * 90000) + 10000
 
 // https://vitejs.dev/config/
+const proxyTarget = process.env.V4_CONFIG || 'http://localhost:8080'
+
 export default defineConfig({
-   define: {
-      // enable hydration mismatch details in production build
-      __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'true'
-   },
-   plugins: [vue()],
-   resolve: {
-      alias: {
-         '@': fileURLToPath(new URL('./src', import.meta.url))
-      }
-   },
-   build: {
+  define: {
+    // enable hydration mismatch details in production build
+    __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'true'
+  },
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  build: {
     rollupOptions: {
       output: {
         entryFileNames: `[name]` + hash + `.js`,
@@ -26,64 +28,64 @@ export default defineConfig({
         assetFileNames: `[name]` + hash + `.[ext]`
       }
     }
-   },
-   server: { // this is used in dev mode only
-      proxy: {
-         '/api': {
-            target: process.env.V4_CONFIG, // export V4_CONFIG=http://localhost:8095
-            changeOrigin: true,
-            logLevel: 'debug'
-          },
-          '/authenticate/netbadge': {
-            target: process.env.V4_CONFIG,
-            changeOrigin: true,
-            logLevel: 'debug'
-          },
-          '/authenticate/public': {
-            target: process.env.V4_CONFIG,
-            changeOrigin: true,
-            logLevel: 'debug'
-          },
-          '/authorize': {
-            target: process.env.V4_CONFIG,
-            changeOrigin: true,
-            logLevel: 'debug'
-          },
-          '/config': {
-            target: process.env.V4_CONFIG,
-            changeOrigin: true,
-            logLevel: 'debug'
-          },
-          '/healthcheck': {
-            target: process.env.V4_CONFIG,
-            changeOrigin: true,
-            logLevel: 'debug'
-          },
-          '/metrics': {
-            target: process.env.V4_CONFIG,
-            changeOrigin: true,
-            logLevel: 'debug'
-          },
-          '/signout': {
-            target: process.env.V4_CONFIG,
-            changeOrigin: true,
-            logLevel: 'debug'
-          },
-          '/version': {
-            target: process.env.V4_CONFIG,
-            changeOrigin: true,
-            logLevel: 'debug'
-          },
-      }
-   },
-   css: {
-      preprocessorOptions : {
-          scss: {
-              api: "modern-compiler",
-              additionalData: `@use "@/assets/theme/colors.scss" as *;`
-          },
-      }
-   },
+  },
+  server: { // this is used in dev mode only
+    proxy: {
+      '/api': {
+        target: proxyTarget, // export V4_CONFIG=http://localhost:8095
+        changeOrigin: true,
+        logLevel: 'debug'
+      },
+      '/authenticate/netbadge': {
+        target: proxyTarget,
+        changeOrigin: true,
+        logLevel: 'debug'
+      },
+      '/authenticate/public': {
+        target: proxyTarget,
+        changeOrigin: true,
+        logLevel: 'debug'
+      },
+      '/authorize': {
+        target: proxyTarget,
+        changeOrigin: true,
+        logLevel: 'debug'
+      },
+      '/config': {
+        target: proxyTarget,
+        changeOrigin: true,
+        logLevel: 'debug'
+      },
+      '/healthcheck': {
+        target: proxyTarget,
+        changeOrigin: true,
+        logLevel: 'debug'
+      },
+      '/metrics': {
+        target: proxyTarget,
+        changeOrigin: true,
+        logLevel: 'debug'
+      },
+      '/signout': {
+        target: proxyTarget,
+        changeOrigin: true,
+        logLevel: 'debug'
+      },
+      '/version': {
+        target: proxyTarget,
+        changeOrigin: true,
+        logLevel: 'debug'
+      },
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: "modern-compiler",
+        additionalData: `@use "@/assets/theme/colors.scss" as *;`
+      },
+    }
+  },
 })
 
 
