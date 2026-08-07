c05c602b8900603575056383d42736e7372590be
Michael Durbin <md5wz@virginia.edu>
2026-08-07 16:46:06 -0400

    Revert "Reapply "Ported 9689b8583d3d4e56a94363a9d4e18d99fd698887""
    This reverts commit 1f7d1f8ae9b92ebe4fe1ef8fcb4d84f1b3cab8fe.

--- frontend/src/stores/item.js
+++ frontend/src/stores/item.js
@@ -12,8 +12,7 @@ export const useItemStore = defineStore('item', {
       digitalContent: [],
       loadingDigitalContent: false,
       availability: {searching: true, titleId: "", libraries: [], bound_with: [], error: ""},
-      primaryFields: ["author", "format", "published_date", "subject", "subject_summary"],
-      noAuthorization: false,
+      primaryFields: ["author", "format", "published_date", "subject", "subject_summary"]
    }),
 
    getters: {
@@ -268,7 +267,6 @@ export const useItemStore = defineStore('item', {
 
       async getDetails( source, identifier ) {
          this.details.searching = true
-         this.noAuthorization = false
 
          // get source from poolID
          const poolStore = usePoolStore()
@@ -321,9 +319,7 @@ export const useItemStore = defineStore('item', {
             }
             this.details.searching = false
          }).catch( async (error) => {
-			if ( error.response && error.response.status == 401) {
-				this.noAuthorization = true
-            } else if ( error.response && error.response.status == 404) {
+            if ( error.response && error.response.status == 404) {
                console.warn(`Item ID ${identifier} not found in ${source}; try a lookup`)
                await this.lookupCatalogKeyDetail(identifier)
             } else {
