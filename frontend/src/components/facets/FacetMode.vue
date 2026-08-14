23e09c230a7fe49b78cf2ba61d918ce89aef48ba
Lou Foster <louffoster@gmail.com>
2026-08-14 14:39:09 -0400

    dont try to persist filter operation changes for guest users

--- frontend/src/components/facets/FacetMode.vue
+++ frontend/src/components/facets/FacetMode.vue
@@ -14,14 +14,18 @@
 import { usePreferencesStore } from "@/stores/preferences"
 import { useResultStore } from "@/stores/result"
 import { useFilterStore } from "@/stores/filter"
+import { useUserStore } from "@/stores/user"
 
+const user = useUserStore()
 const filters = useFilterStore()
 const resultStore = useResultStore()
 const preferences = usePreferencesStore()
 
 const modeChanged = (async () => {
    console.log(preferences.facetMode)
-   preferences.updateFacetJoinMode()
+   if ( user.isSignedIn ) {
+      preferences.updateFacetJoinMode()
+   }
    if (resultStore.hasResults) {
       filters.setDirty()
       await resultStore.searchAllPools()
